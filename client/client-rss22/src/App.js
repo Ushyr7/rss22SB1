874fcb655b7ac7ee6bc3d2f69e80fa8c9005af43
unknown <quentin.ridel@etu.univ-rouen.fr>
2022-04-29 19:05:10 +0200

    finalisation du client

--- client/client-rss22/src/App.js
+++ client/client-rss22/src/App.js
@@ -5,7 +5,12 @@ import React, { useState, useEffect } from "react"
 function App() {
   const [InputValueGet, setInputValueGet] = useState("");
   const [InputValueRemove, setInputValueRemove] = useState("");
+  const [InputValueAdd, setInputValueAdd ] = useState("");
   const [ListArticle, setListArticle] = useState("");
+
+  const[Article, setArticle] = useState("");
+  const[ArticleToDelete, setArticleToDeleteResponse] = useState("");
+  const[FluxToInsert, setFluxResponse] = useState("");
 
   useEffect(()=>{
     axios.get("/rss22/resume/html", {"application" : "text/html"}).then(response => {
@@ -21,6 +26,48 @@ function App() {
   const getInputValueGetArticle = (event)=>{
     setInputValueGet(event.target.value);
   };
+
+  const getInputValueAddArticle = (event) => {
+    setInputValueAdd(event.target.value);
+  }
+
+  const handleGetArticle = () => {
+    axios.get("/rss22/resume/html/" + InputValueGet, {"application" : "text/html"}).catch(function (error) {
+      if (error.response) {
+        setArticle("Erreur " + error.response.status + "... " + error.response.data);
+      }
+    }).then(response => {
+      if(InputValueGet !== "") {
+        setArticle(response.data);
+      }
+    });
+  }
+
+  const handleDeleteArticle = () => {
+    axios.delete("/rss22/delete/" + InputValueRemove).catch(function (error) {
+      if (error.response) {
+        setArticleToDeleteResponse( error.response.status + "... " + error.response.data);
+      }
+    }).then(response => {
+      if(InputValueRemove !== "") {
+        setArticleToDeleteResponse(response.data + " supprimé !");
+      }
+    });
+  }
+
+  const handleInsert = () => {
+
+    axios.post("/rss22/insert",InputValueAdd, {headers: {'Content-Type': 'application/xml'}}).catch(function (error) {
+      if (error.response) {
+        setFluxResponse( "Erreur " + error.response.status);
+      }
+    }).then(response => {
+      if(InputValueAdd !== "") {
+        setFluxResponse(response.data);
+      }
+    });
+    console.log("In")
+  }
 
   return (
     
@@ -43,9 +90,8 @@ function App() {
         <div class="card-content black-text">
           <span class="card-title">Obtenir un article :</span>
             <div>
-              <input placeholder="Guid de l'article à lire"type="text" onChange={getInputValueGetArticle}></input>
-              <p>{InputValueGet}</p>
-              <button class="btn green darken-2">Obtenir</button>
+              <input placeholder="Guid de l'article à lire"type="text" required onChange={getInputValueGetArticle}></input>
+              <button class="btn green darken-2" onClick={handleGetArticle}>Obtenir</button>
             </div>
         </div>
       </div>
@@ -56,16 +102,35 @@ function App() {
         <div class="card-content black-text">
           <span class="card-title">Supprimer un article :</span>
           <div>
-            <input placeholder="Guid de l'article à supprimer" type="text" onChange={getInputValueRemoveArticle}></input>
-            <p>{InputValueRemove}</p>
-            <button class="btn green darken-2">Supprimer</button>
+            <input placeholder="Guid de l'article à supprimer" type="text" required onChange={getInputValueRemoveArticle}></input>
+            <button class="btn green darken-2" onClick={handleDeleteArticle}>Supprimer</button>
+            <div class="container" dangerouslySetInnerHTML={{__html: ArticleToDelete}}/>
           </div>
         </div>
       </div>
     </div>
   </div>
 
+  <div class="card white">
+        <div class="card-content black-text">
+            <div
+              dangerouslySetInnerHTML={{__html: Article}}
+            />
+        </div>
+      </div>
   <div class="container" dangerouslySetInnerHTML={{__html: ListArticle}}/>
+  <div class="col s12 m6">
+      <div class="card grey lighten-4">
+        <div class="card-content black-text">
+          <span class="card-title">Ajouter un article en format XML : </span>
+          <div>
+            <input type="text" required onChange={getInputValueAddArticle}></input>
+            <button class="btn green darken-2" onClick={handleInsert}>ajouter</button>
+            <div>{FluxToInsert}</div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 </body>
 </html>
